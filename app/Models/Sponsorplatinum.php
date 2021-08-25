545fcd27db8aab5edab66d3148132dc6cb4c1591
shabantech <shabaan@techinvestkw.com>
2021-08-25 22:16:25 +0300

    fix

--- app/Models/Sponsorplatinum.php
+++ app/Models/Sponsorplatinum.php
@@ -84,7 +84,7 @@ class Sponsorplatinum extends Model
     public function getImageAttribute($value)
     {
         if ($value != null && $value != '') {
-            return asset('uploads/folder_1/folder_2' . $value);
+            return asset('uploads/folder_1/folder_2/' . $value);
         }
         return $value;
     }
