6e91fe2937d6b72c030278def094ac4a8044af90
mohamedshaaban <mohammad.shabaan@gmail.com>
2021-09-03 10:24:01 +0000

    fix

--- config/app.php
+++ config/app.php
@@ -155,6 +155,7 @@ return [
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\KnetServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
         /*
