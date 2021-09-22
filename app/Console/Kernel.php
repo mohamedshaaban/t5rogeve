468b36ea65f6bf4ea1fbada3e80343fef72c9d20
shabantech <shabaan@techinvestkw.com>
2021-09-22 22:54:36 +0300

    updates

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,6 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command('createxerotoken')->everyMinute();
         $schedule->command('db:fresh')->hourly();
     }
 }
